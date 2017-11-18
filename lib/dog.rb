7c214204681be9ebe48f8c0c0a6b4970ea5c722e
Phoebe Lau <phoebelau7@gmail.com>
2017-11-18 01:39:47 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,12 @@
 class Dog
-  def set(name)
-    @name = name
-  end;
-  def name = (dog_name)
-    @name = dog_name
+  def breed(dog_breed)
+    @dog_breed = dog_breed
+  end
+
+  def breed
+    
+  end
+
+  def breed = dog_breed
   end
 end
